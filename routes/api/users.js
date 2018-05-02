961e8a6a53bfdbd65eb41763a432cd6c1ff5923c
Travis Jones <travisj_jones@hotmail.com>
2018-05-02 11:35:13 -0500

    added jwt token to create bearer token for passportjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,15 +23,21 @@ const User = require("../../models/User");
 // @route    GET api/users/test
 // @desc     Tests users route
 // @access   Public
-router.get("/test", (req, res) => res.json({ msg: "Users works!" }));
+router.get("/test", (req, res) => res.json({
+  msg: "Users works!"
+}));
 
 // @route    GET api/users/register
 // @desc     Register User
 // @access   Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "email already exists" });
+      return res.status(400).json({
+        email: "email already exists"
+      });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", // Size
@@ -68,25 +74,41 @@ router.post("/login", (req, res) => {
   const password = req.body.password;
 
   // Find user by email
-  User.findOne({ email }).then(user => {
+  User.findOne({
+    email
+  }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      return res.status(404).json({
+        email: "User not found"
+      });
     }
 
     // Check password -- Password from req.body && hashed password from DB
     bcrypt.compare(password, user.password).then(isMatch => {
       // Passes back a boolean is passwords match
-      if (isMatch) {
-        // User matched
-        res.json({ msg: "Success!" });
-
+      if (isMatch) { // User matched
         // Payload for JWT
-        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
         // Sign Token
-        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 });
+        jwt.sign(
+          payload,
+          keys.secretOrKey, 
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            })
+          });
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        return res.status(400).json({
+          password: "Password incorrect"
+        });
       }
     });
   });
