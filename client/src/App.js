29f7310a5e7d7d625614d4c555d45362be6227b0
Travis Jones <travisj_jones@hotmail.com>
2018-05-07 14:14:43 -0500

    created Routes for login and register pages and home page. changed hrefs and a tags to Link and to

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./Components/layout/Navbar";
 import Footer from "./Components/layout/Footer";
 import Landing from "./Components/layout/Landing";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="conatiner">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
