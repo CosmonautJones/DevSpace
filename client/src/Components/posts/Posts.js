537d236a5ed37b79d4573593413a0e220a23b2fa
Travis Jones <travisj_jones@hotmail.com>
2018-05-11 14:21:55 -0500

    set up loading function to post feed and built the action to getPosts

--- client/src/Components/posts/Posts.js
+++ client/src/Components/posts/Posts.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
+import { getPosts } from '../../actions/postActions';
 
 class Posts extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
     return (
       <div className="feed">
         <div className="container">
@@ -19,4 +33,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
